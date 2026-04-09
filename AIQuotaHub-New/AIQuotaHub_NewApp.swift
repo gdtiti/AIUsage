ed7581b0dc4f280528b958379b20fa065cf1b562
sylearn <sylearn@foxmail.com>
2026-04-09 17:43:17 +0800

    Initial Commit

---
+++ AIQuotaHub-New/AIQuotaHub_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIQuotaHub_NewApp.swift
+//  AIQuotaHub-New
+//
+//  Created by sylearn on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct AIQuotaHub_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
